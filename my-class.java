4c1a6697f5040bfa5d40f5efe3c29f70bae1b8ef
Tulis <79586741+Tulis12@users.noreply.github.com>
2025-09-10 09:32:43 +0200

    Create my-class.java

---
+++ my-class.java
@@ -0,0 +1,9 @@
+public class Tulis extends Human {
+  String name = "Tulis";
+  String role = "student";
+  
+  List<> learning = List.of("Spring framework", "C++", "Mashine Learning");
+  List<> projects = List.of("Tulisiowice", "Cubecopia", "Blogee", "TuliTrackMaster");
+  List<> programmingLangs = List.of("Java <3", "Python", "JS", "AngelScript");
+  List<> favouriteLibraries = List.of("Spring Boot", "jOOQ", "Spigot-API", "PyTorch");
+}
