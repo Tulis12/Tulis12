25a5ff46f18ec9604ca110432014eb6cde3c47e1
Tulis <79586741+Tulis12@users.noreply.github.com>
2025-09-17 09:07:11 +0200

    update spelling mistake, change tulitrackmaster to tulimania

--- my-class.java
+++ my-class.java
@@ -1,9 +1,11 @@
+import java.util.List;
+
 public class Tulis extends Human {
   String name = "Tulis";
   String role = "student";
   
-  List<> learning = List.of("Spring framework", "C++", "Mashine Learning");
-  List<> projects = List.of("Tulisiowice", "Cubecopia", "Blogee", "TuliTrackMaster");
+  List<> learning = List.of("Spring framework", "C++", "Machine Learning");
+  List<> projects = List.of("Tulisiowice", "Cubecopia", "Blogee", "TuliMania");
   List<> programmingLangs = List.of("Java <3", "Python", "JS", "AngelScript");
   List<> favouriteLibraries = List.of("Spring Boot", "jOOQ", "Spigot-API", "PyTorch");
 }
